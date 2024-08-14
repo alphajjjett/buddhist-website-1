1f316544d8c9e829c0dcb27148780cf26361e1f7
Suttipat Joysrithong <jetbrabore@hotmail.com>
2024-08-14 22:35:18 +0700

    update_01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   
@@ -13,10 +15,32 @@ function App() {
           <li><a href="about">ทำ</a></li>
           <li><a href="services">ไม</a></li>
         </ul>
-        <body className="body-data">
-
-        </body>
         </nav>
+  
+    <Card style={{ width: '100%' }}>
+      <Card.Body className="d-flex align-items-center">
+        <img 
+          src="https://dz.lnwfile.com/ae26i6.jpg" 
+          alt="Cardimg" 
+          className="card-img-left" 
+          />
+        <div>
+          <Card.Title>
+            <h1>แต่ละมื้อแต่ละ Day</h1>
+          </Card.Title>
+          <Card.Text>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. 
+              Per aliquam vivamus aliquet egestas purus est condimentum morbi penatibus? Dolor nibh scelerisque interdum purus dis. 
+              Magna ad aptent aliquet consequat massa adipiscing litora elit.
+             At lectus aptent nullam placerat vitae cubilia. Netus sociosqu etiam lacus est tellus.
+            </p>
+          </Card.Text>
+          <Button> อ่านเพิ่มเติม </Button>
+        </div>
+      </Card.Body>
+    </Card>
+        
     </div>
    
   );
