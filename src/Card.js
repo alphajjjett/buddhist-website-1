5107b52fd12cb1604c6d014a0ef8ab0c1c02101a
Suttipat Joysrithong <jetbrabore@hotmail.com>
2024-08-19 22:59:24 +0700

    commit

--- src/Card.js
+++ src/Card.js
@@ -1,33 +1,65 @@
 import React from 'react';
-import './Card.css';
+import './component/card/Card.css';
 
 function Card() {
   return (
-    <div className='BOX'>
-        <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-                <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                    class="h-56 w-full object-cover"
-                />
-
-                <div class="bg-white p-4 sm:p-6">
-                    <time datetime="2022-10-10" class="block text-xs text-gray-500"> 10th Oct 2022 </time>
-
-                    <a href="#">
-                    <h3 class="mt-0.5 text-lg text-gray-900">How to position your furniture for positivity</h3>
-                    </a>
-
-                    <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-                    pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
-                    mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
-                    dignissimos. Molestias explicabo corporis voluptatem?
-                    </p>
-                </div>
+    <div className='container'>
+      <div className='BOX1'>
+        <article className="card">
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            className="card-image"
+          />
+          <div className="card-content">
+            <time dateTime="2022-10-10" className="card-date"> 10th Oct 2022 </time>
+            <a href="#">
+              <h3 className="card-title">How to position your furniture for positivity</h3>
+            </a>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
         </article>
+      </div>
+      <div className='BOX2'>
+        <article className="card">
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            className="card-image"
+          />
+          <div className="card-content">
+            <time dateTime="2022-10-11" className="card-date"> 11th Oct 2022 </time>
+            <a href="#">
+              <h3 className="card-title">Tips for creating a productive workspace</h3>
+            </a>
+            <p className="card-text">
+              Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium.
+            </p>
+          </div>
+        </article>
+      </div>
+      <div className='BOX3'>
+        <article className="card">
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            className="card-image"
+          />
+          <div className="card-content">
+            <time dateTime="2022-10-12" className="card-date"> 12th Oct 2022 </time>
+            <a href="#">
+              <h3 className="card-title">Tips for creating a productive workspace</h3>
+            </a>
+            <p className="card-text">
+              Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium.
+            </p>
+          </div>
+        </article>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
