f844eb93092f2547103e3376bfaba5a12dd04aa3
Suttipat Joysrithong <jetbrabore@hotmail.com>
2024-08-29 13:00:20 +0700

    commit

--- src/component/footer/Fterpage.js
+++ src/component/footer/Fterpage.js
@@ -6,8 +6,7 @@ function Fterpage() {
       className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
       <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
         © 2024 Copyright :
-        <a href="https://www.github.com/alphajjjett" aria-label='Github' 
-            rel="noreferrer" target="_blank"
+        <a href="https://www.github.com/alphajjjett"
             className="text-neutral-800 dark:text-neutral-400">
               Suttipat-CS54
         </a>
